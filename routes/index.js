e4b9d6b0530eb6e90fd9f65922181ceb54b2f236
Andy Smith <andy@ymkm.com>
2014-08-17 22:55:10 +1000

    hello world

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+
+if (process.env.REDISTOGO_URL) {
+    var rtg = require("url").parse(process.env.REDISTOGO_URL);
+    var redis = require("redis").createClient(rtg.port, rtg.hostname);
+    redis.auth(rtg.auth.split(":")[1]);
+} else {
+    var redis = require("redis").createClient();
+}
+
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+    redis.set("hello", "world");
+
+    redis.get('hello', function (error, result) {
+        if (error) console.log('Error: ' + error);
+        else  res.render('index', { title: result });
+    });
+
 });
 
 module.exports = router;
